f68a49e9f7bf1893e8ab0c7b06ca02c7edb5a5aa
Amy Lam <amy@Marcos-MBP.hsd1.ca.comcast.net>
2021-09-17 09:20:22 -0700

    Added Login and Logout feature

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import PrivateRoute from './components/PrivateRoute';
 import Login from "./components/Login";
+import BubblePage from './components/BubblePage'
 import "./styles.scss";
+import axiosWithAuth from "./helpers/axiosWithAuth";
 
 function App() {
+
+  const logout = () => {
+    axiosWithAuth().post('/logout')
+      .then(resp => {
+        localStorage.removeItem("token");
+        window.location.href = "/login";
+      }).catch(err => {
+        console.log(err);
+      });
+  };
+
   return (
     <Router>
       <div className="App">
         <header>
           Color Picker Sprint Challenge
-          <a data-testid="logoutButton" href="#">logout</a>
+          <button data-testid="logoutButton" onClick={logout}>logout</button>
         </header>
+        <Switch>
+          <PrivateRoute path="/bubble" component={BubblePage} />
+          <Route path="/login" component={Login} />
+          <Route path="/" component={Login} />
+        </Switch>
       </div>
     </Router>
   );
@@ -22,4 +40,4 @@ export default App;
 //Task List:
 //1. Add in two routes that link to the Login Component, one for the default path '/' and one for the '/login'.
 //2. Render BubblePage as a PrivateRoute
-//2. Build the logout button to call the logout endpoint, remove the localStorage Item and redirect to the login page.
+//2. Build the logout button to call the logout endpoint, remove the localStorage It 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
+import { useHistory } from "react-router";
 
 const Login = () => {
+  const [user, setUser] = useState({
+    credentials: {
+      username: '',
+      password: ''
+    }
+  })
+  const [error, setError] = useState(false);
+  const { push } = useHistory();
+
+  const handleChange = e => {
+    setUser({
+      credentials: {
+        ...user.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
 
-  const error = "";
+  const login = e => {
+    e.preventDefault();
+    if (user.credentials.username === "" || user.credentials.password === "") {
+      setError("Username or Password not valid.");
+    } else {
+      axios.post("http://localhost:5000/api/login", user.credentials)
+        .then(resp => {
+          setError("");
+          localStorage.setItem("token", resp.data.payload);
+          push('/bubble');
+      })
+      .catch(err => {
+        console.log(err);
+      })
+    }
+  };
+
+  // const error = "";
   //replace with error state
 
   return (
     <div>
       <h1>Welcome to the Bubble App!</h1>
       <div data-testid="loginForm" className="login-form">
-        <h2>Build login form here</h2>
+      <form onSubmit={login}>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            value={user.credentials.username}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={user.credentials.password}
+            onChange={handleChange}
+          />
+          <button id="submit">Log in</button>
+        </form>
       </div>
 
       <p id="error" className="error">{error}</p>
